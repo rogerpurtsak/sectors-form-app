a39bd8b8ae803c357bd3f3ad38239bae0e2fcd26
ropurt <ropurt@taltech.ee>
2026-06-03 14:02:12 +0300

    frontend form and loading sectors from backend

--- frontend/src/app/components/sector-form/sector-form.component.ts
+++ frontend/src/app/components/sector-form/sector-form.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SectorService } from '../../services/sector.service';
+import { Sector } from '../../models/sector.model';
 
 @Component({
   selector: 'app-sector-form',
   imports: [ReactiveFormsModule],
   templateUrl: './sector-form.component.html',
-
 })
-export class SectorFormComponent {}
+export class SectorFormComponent implements OnInit {
+  // this goes to the html for loop
+  sectors: Sector[] = [];
+
+  name         = new FormControl('',    Validators.required);
+  sectorIds    = new FormControl<number[]>([], Validators.required);
+  agreeToTerms = new FormControl(false, Validators.requiredTrue);
+
+  form = new FormGroup({
+    name:         this.name,
+    sectorIds:    this.sectorIds,
+    agreeToTerms: this.agreeToTerms,
+  });
+
+  constructor(private sectorService: SectorService) {}
+
+  ngOnInit(): void {
+    this.sectorService.getAll().subscribe(sectors => {
+      this.sectors = sectors;
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      console.log('Form value:', this.form.value);
+    }
+  }
+
+  indentedName(sector: Sector): string {
+    return ' '.repeat(sector.level * 4) + sector.name;
+  }
+}
